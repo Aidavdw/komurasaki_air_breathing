752f84d24438718aa156224f5a857d4a9f75cb1f
Aidavdw (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2023-03-13 18:09:21 +0900

    Moved initialisation of FieldQuantity to its own function
    This way it can be re-used for the actual initialisation.

--- field_quantity.cpp
+++ field_quantity.cpp
@@ -1,12 +1,17 @@
 #include "field_quantity.h"
 
-FieldQuantity::FieldQuantity(const unsigned int sizeX, const unsigned int sizeY, const bool bCell, const double initalValue)
+FieldQuantity::FieldQuantity(const unsigned int sizeX, const unsigned int sizeY, const bool bCell, const double initialValue)
 {
 	// if it's set to be a cell, apparently according to florian it needs an additional element.
 	std::vector<double> single_row(sizeX + bCell, 0);
 	field = std::vector<std::vector<double>>(sizeY + bCell, single_row);
 
+	SetAllToValue(initialValue);
+}
+
+void FieldQuantity::SetAllToValue(const double value)
+{
 	for (int i = 0; i < field.size(); i++)
-		for (int j = 0; j < single_row.size(); j++)
+		for (int j = 0; j < field[0].size(); j++)
 			field[i][j] = 0.0;
 }
--- field_quantity.h
+++ field_quantity.h
@@ -6,9 +6,13 @@ struct FieldQuantity
 	FieldQuantity()
 	{};
 
-	FieldQuantity(const unsigned int sizeX, const unsigned int sizeY, const bool bCell=false, const double initalValue = 0.f);
+	FieldQuantity(const unsigned int sizeX, const unsigned int sizeY, const bool bCell=false, const double initialValue = 0.);
 
+	//todo: Investigate how easy it is to switch the x/y order in which field vectors are stored, because this is going to cause a lot of headaches.
+
+	// The value of the field at a specific x/y place. value[x][y]. the first column is the x coordinate, the second one the y coordinate, because of compatibility with legacy compatibility.
 	std::vector<std::vector<double>> field;
-	
 
+	// Sets all the values in the field to this value.
+	void SetAllToValue(const double value);
 };
