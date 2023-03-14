1869b8a460401db6a6f5dd21216a2e6a30597c07
Aidavdw (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2023-03-14 20:06:29 +0900

    Made field quantities have multiple buffers

--- field_quantity.cpp
+++ field_quantity.cpp
@@ -1,17 +1,35 @@
 #include "field_quantity.h"
+#include <stdexcept>
 
 FieldQuantity::FieldQuantity(const unsigned int sizeX, const unsigned int sizeY, const bool bCell, const double initialValue)
 {
 	// if it's set to be a cell, apparently according to florian it needs an additional element.
-	std::vector<double> single_row(sizeX + bCell, 0);
-	field = std::vector<std::vector<double>>(sizeY + bCell, single_row);
-
-	SetAllToValue(initialValue);
+	Resize2DVector(field, sizeX, sizeY, bCell, initialValue);
 }
 
-void FieldQuantity::SetAllToValue(const double value)
+void FieldQuantity::SetAllToValue(const double value, const EFieldQuantityBuffer bufferToWriteTo = EFieldQuantityBuffer::MAIN)
 {
-	for (int i = 0; i < field.size(); i++)
-		for (int j = 0; j < field[0].size(); j++)
-			field[i][j] = 0.0;
+	auto& buffer = bufferMap.at(bufferToWriteTo);
+	SetToValueInternal(buffer, value);
+}
+
+void FieldQuantity::CopyToBuffer(const EFieldQuantityBuffer from, const EFieldQuantityBuffer to)
+{
+	auto& fromBuffer = bufferMap.at(from);
+	auto& toBuffer = bufferMap.at(to);
+
+}
+
+void FieldQuantity::SetToValueInternal(std::vector<std::vector<double>>& Vec2D, const double value)
+{
+	for (int i = 0; i < Vec2D.size(); i++)
+		for (int j = 0; j < Vec2D[0].size(); j++)
+			Vec2D[i][j] = 0.0;
+}
+
+void FieldQuantity::Resize2DVector(std::vector<std::vector<double>>& vec2D, const unsigned int sizeX, const unsigned int sizeY, const bool bCell, const double initialValue)
+{
+	// if it's set to be a cell, apparently according to florian it needs an additional element.
+	std::vector<double> single_row(sizeX + bCell, initialValue);
+	vec2D = std::vector<std::vector<double>>(sizeY + bCell, single_row);
 }
--- field_quantity.h
+++ field_quantity.h
@@ -1,4 +1,11 @@
 #include <vector>
+#include <map>
+
+enum EFieldQuantityBuffer
+{
+	MAIN, // The field that physically represents the value
+	RUNGEKUTTA, // to be used for runge kutta iteration. A temporary buffer.
+};
 
 // Thin wrapper around a 2d array with doubles, representing a value over an entire domain (in a grid)
 struct FieldQuantity
@@ -10,11 +17,19 @@ struct FieldQuantity
 
 	//todo: Investigate how easy it is to switch the x/y order in which field vectors are stored, because this is going to cause a lot of headaches.
 
-	// The value of the field at a specific x/y place. value[x][y]. the first column is the x coordinate, the second one the y coordinate, because of compatibility with legacy compatibility.
-	std::vector<std::vector<double>> field;
-
 	// Todo: implement the ghost cells here!
+	// The value of the field at a specific x/y place. value[x][y]. the first column is the x coordinate, the second one the y coordinate, because of compatibility with legacy compatibility.
+	std::map<EFieldQuantityBuffer, std::vector<std::vector<double>>&> bufferMap;
+	std::vector<std::vector<double>> field;
+	std::vector<std::vector<double>> rungeKuttaBuffer;
 
 	// Sets all the values in the field to this value.
-	void SetAllToValue(const double value);
+	void SetAllToValue(const double value, const EFieldQuantityBuffer bufferToWriteTo=EFieldQuantityBuffer::MAIN);
+
+	void CopyToBuffer(const EFieldQuantityBuffer from, const EFieldQuantityBuffer to);
+
+private:
+	// Sets a specific 2d vector to a value. Useful for internal use to call on the buffers.
+	static void SetToValueInternal(std::vector<std::vector<double>>& valueField, const double value);
+	static void Resize2DVector(std::vector<std::vector<double>>& valueField, const unsigned int sizeX, const unsigned int sizeY, const bool bCell, const double initialValue);
 };
