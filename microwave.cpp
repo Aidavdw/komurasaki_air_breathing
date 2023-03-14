24a40c0d4c23ac620449e6ccff77d9e462f7876e
Aidavdw (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2023-03-14 13:41:07 +0900

    Implemented initialisation for domain based on Detonation Theory
    The implementation is based on what was found in initial.h's appl_initial_conditions

--- microwave.cpp
+++ microwave.cpp
@@ -1,4 +1,5 @@
 #include "microwave.h"
+#include "domain.h"
 #include <math.h>
 #include <stdexcept>
 
@@ -13,18 +14,17 @@ double eval_msd_deriv(double x, double c)
     return -c / pow(x, 2) * (0.5 * sqrt(c / x + 1) + 0.5 * sqrt(c / x)) - 1;
 }
 
-ChapmanJougetDetonationSolution SolveChapmanJougetDetonationProblem(const double T0, const double P0, const double ETA, const double S0, const double R, const double GAMMA, const double L_TUBE, const double R0, const double convergenceThreshold)
+ChapmanJougetDetonationSolution SolveChapmanJougetDetonationProblem(const double T0, const double P0, const double ETA, const double S0, const double R_ideal, const double GAMMA, const double L_TUBE, const double RadiusOfCombustionTube, const double convergenceThreshold)
 {
-    CellValues postDetonation;          // The values of the flow after the detonation
     CellValues postExpansion;           // The values of the flow after the expansion
 
     // todo: This value was defined in main.c, not sure where it came from. Move to different scope?
     const static int M_PI = 4.0 * atan(1.0);
     const static int MAX_ITERS = 200;
 
-    double rho0 = P0 / (R * T0); // Ambient density
-    double a0 = sqrt(GAMMA * R * T0); // Ambient speed of sound
-    double Sd = ETA * S0 / (M_PI * R0 * R0); // Mean power density
+    double rho0 = P0 / (R_ideal * T0); // Ambient density
+    double a0 = sqrt(GAMMA * R_ideal * T0); // Ambient speed of sound
+    double Sd = ETA * S0 / (M_PI * RadiusOfCombustionTube * RadiusOfCombustionTube); // Mean power density
 
     //printf("Microwave surface power density is: %f MW/cm^2.\n",Sd/1.0E6/100.0/100.0);
     // printf("Ionization velocity is: %f m/s.\n",4190.0*Sd/1.0E6/100.0/100.0-14.9);
@@ -62,28 +62,33 @@ ChapmanJougetDetonationSolution SolveChapmanJougetDetonationProblem(const double
     //printf("Detonation velocity with ETA=0.49 is: %f m/s.\n",Meta*a0);
     */
 
-    //TODO: I think this is the post-detonation mach number, but it could be the post-expansion mach number to. Check which one it is.
+    // This is equation 3.11 from Florian (2017)
     double m1 = (pow(Mnext, 2) - 1.0) / (1.0 + GAMMA * pow(Mnext, 2));
 
-    // Post-detonation conditions
-    postDetonation.p = (1.0 + GAMMA * pow(Mnext, 2)) / (GAMMA + 1) * P0;
-    postDetonation.u = a0 * Mnext * (pow(Mnext, 2.0) - 1.0) / pow(Mnext, 2.0) / (GAMMA + 1.0);
-    postDetonation.rho = (1.0 + GAMMA) * pow(Mnext, 2) / (1.0 + GAMMA * pow(Mnext, 2)) * rho0;
+    // Post-detonation conditions, immediately behind the detonation front (subscript 1). These are required for calculating the plateau conditions, and afterwards discarded. The calculation of the values behind the shockwave is x-location dependent, and is defined in ChapmanJougetDetonationSolution::FieldPropertiesAtPosition()
+    double p1 = (1.0 + GAMMA * pow(Mnext, 2)) / (GAMMA + 1) * P0;
+    double u1 = a0 * Mnext * (pow(Mnext, 2.0) - 1.0) / pow(Mnext, 2.0) / (GAMMA + 1.0);
+    double rho1 = (1.0 + GAMMA) * pow(Mnext, 2) / (1.0 + GAMMA * pow(Mnext, 2)) * rho0;
 
-    // Post-expansion wave conditions
-    postExpansion.p = pow(1.0 - 0.5 * (GAMMA - 1) * (m1), 2.0 * GAMMA / (GAMMA - 1)) * (postDetonation.p);
-    postExpansion.rho = pow((1.0 - 0.5 * (GAMMA - 1) * (m1)), 2.0 / (GAMMA - 1)) * (postDetonation.rho);
+
+    // Post-expansion wave conditions subscript 2
+    postExpansion.p = pow(1.0 - 0.5 * (GAMMA - 1) * (m1), 2.0 * GAMMA / (GAMMA - 1)) * (p1);
+    postExpansion.rho = pow((1.0 - 0.5 * (GAMMA - 1) * (m1)), 2.0 / (GAMMA - 1)) * (rho1);
     double a2 = (1.0 - 0.5 * (GAMMA - 1.0) * (m1)) / (GAMMA + 1.0) * (pow(Mnext, 2) * GAMMA + 1.0) / pow(Mnext, 2) * Mnext * a0;
+    postExpansion.T = postExpansion.p / (postExpansion.rho * R_ideal);
+    postExpansion.u = 0;
+    postExpansion.v = 0;
+    postExpansion.E = postExpansion.p / (GAMMA - 1.0) + 0.5 * postExpansion.rho * (pow(postExpansion.u, 2) + pow(postExpansion.v, 2)); // since u & v are 0, these terms drop out entirely.
+    postExpansion.H = (postExpansion.E + postExpansion.p) / postExpansion.rho;
 
 
 
-    ChapmanJougetDetonationSolution solution = ChapmanJougetDetonationSolution(postDetonation, postExpansion);
+    ChapmanJougetDetonationSolution solution = ChapmanJougetDetonationSolution(postExpansion);
     solution.m_msd = Mnext;
     solution.m1 = m1;
     // Computing position of expansion wave front and rear
     solution.l_exp = (L_TUBE / Mnext / a0) * a2;
     solution.detonation_velocity = Mnext * a0;
-
 
     // printf("\nMean Microwave Power is: %f W/m^2.\n",Sd);
     //printf("Detonation velocity is: %f m/s (M_MSD = %f).\n", Mnext * a0, Mnext);
@@ -95,3 +100,56 @@ ChapmanJougetDetonationSolution SolveChapmanJougetDetonationProblem(const double
     return solution;
 }
 
+void InitialiseDomainFromChapmanJougetDetonationSolution(Domain* domain, const ChapmanJougetDetonationSolution& sol, const double gamma)
+{
+    //Todo: Take this parameter out, so that it can also be made standing up.
+    const double tubeRadius = domain->size[1]; // For now, assume a laying down tube. 
+    // As the solution is 1D, the solution is the same across all the y - coordinates, and is only different for x coordinates. Hence, iterate over the x- coordinates and set it immediately for all y coordinates.
+    for (int xIndex = 0; xIndex < domain->size[0]; xIndex++)
+    {
+        double xPos = domain->meshSpacing[0].GetCellPosition(xIndex);
+        CellValues cellValues = sol.FieldPropertiesAtPosition(xPos, gamma, tubeRadius);
+        
+        for (int yIndex = 0; yIndex < domain->size[1]; yIndex++)
+        {
+            domain->rho.SetAllToValue(cellValues.rho);
+            domain->u.SetAllToValue(cellValues.u);
+            domain->v.SetAllToValue(cellValues.v);
+            domain->p.SetAllToValue(cellValues.p);
+            domain->E.SetAllToValue(cellValues.E);
+            domain->T.SetAllToValue(cellValues.T);
+            domain->E.SetAllToValue(cellValues.E);
+        }
+    }
+}
+
+CellValues ChapmanJougetDetonationSolution::FieldPropertiesAtPosition(const double xPosition, const double gamma, const double tubeRadius) const
+{
+    // See if this is the plateau region or if this is the region where stuff gradually increases.
+    /*     |           /
+    *  P,T |          /
+    *  U   |         /
+    *  rho | -------+
+    *      |
+    *      +------------
+    *        post   ^  inside expansion
+    *             l_exp
+    */
+    
+    if (xPosition <= l_exp)
+    {
+        return postExpansion;
+    }
+    else // It's in the expansion region- Note that the solution is only valid up until the actual x position at the end of the tube.
+    {
+        CellValues c;
+        c.p = postExpansion.p* pow(1.0 - (gamma - 1.0) / (gamma + 1.0) * (1.0 - xPosition / l_exp), 2.0 * gamma / (gamma - 1.0));
+        c.rho = postExpansion.rho * pow(postExpansion.p / postExpansion.p, 1.0 / gamma);
+        c.T = postExpansion.p / (postExpansion.rho * tubeRadius) * pow(c.rho / postExpansion.rho, gamma - 1.0);
+        c.u = -2.0 / (gamma - 1.0) * (sqrt(gamma * postExpansion.p / postExpansion.rho) - sqrt(gamma * tubeRadius * c.T));
+        c.v = 0.0;
+
+        c.E = c.p / (gamma - 1.0) + 0.5 * c.rho * (pow(c.u, 2) + pow(c.v, 2));
+        c.H= (c.E + c.p) / c.rho;
+    }
+}
--- microwave.h
+++ microwave.h
@@ -6,12 +6,12 @@
 */
 
 #include "cell_values_container.h"
+class Domain;
 
 // Represents a solution to a 1-d Chapman-Jouget detonation problem
 struct ChapmanJougetDetonationSolution
 {
-    ChapmanJougetDetonationSolution(const CellValues& postDetonation, const CellValues& postExpansion) :
-        postDetonation(postDetonation),
+    ChapmanJougetDetonationSolution(const CellValues& postExpansion) :
         postExpansion(postExpansion)
     {
 
@@ -23,16 +23,18 @@ struct ChapmanJougetDetonationSolution
     double m1 = 0;                      // post-detonation mach number
     double detonation_velocity = 0;     // Velocity of the detonation front
 
-    CellValues postDetonation;          // The values of the flow after the detonation
-    CellValues postExpansion;           // The values of the flow after the expansion
+    //CellValues postDetonation;          // Post-detonation conditions, immediately behind the detonation front. This is what holds at the thrust wall. (subscript 1). Not serialised as of now.
+    CellValues postExpansion;           // Post-expansion conditions. This is what 'creeps in' from the rear, going towards the thrust wall.
 
-    double p1 = 0;                      // post-detonation pressure
-    double u1 = 0;                      // post-detonation speed ? not sure what it represents
-    double rho1 = 0;                    // post-detonation density
+    //double p1 = 0;                      // post-detonation pressure
+    //double u1 = 0;                      // post-detonation speed ? not sure what it represents
+    //double rho1 = 0;                    // post-detonation density
 
-    double p2 = 0;                      // post-expansion pressure
-    double rho2 = 0;                    // post-expansion density
+    //double p2 = 0;                      // post-expansion pressure
+    //double rho2 = 0;                    // post-expansion density
     double l_exp = 0;                   // The position of the tail of the expansion region
+
+    CellValues FieldPropertiesAtPosition(const double xPosition, const double gamma, const double tubeRadius) const;
 };
 
 /* Function to solve to find MSD Mach number */
@@ -41,7 +43,9 @@ double eval_msd_function(double x, double c);
 /* Derivative of function to solve to find MSD Mach number */
 double eval_msd_deriv(double x, double c);
 
-//todo: rename R0 parameter, as its confusing. I think that R0 here is the radius of the tube!
+//todo: rename R0 parameter, as its confusing.
 /* Compute initial pressure, density and temperature conditions based on Newton-Raphson's method.  */
 ChapmanJougetDetonationSolution SolveChapmanJougetDetonationProblem(const double T0, const double P0, const double ETA, const double S0, const double R, const double GAMMA, const double L_TUBE, const double R0, const double convergenceThreshold = 1.0E-10);
 
+// Populates a domain with the values in a Chapman-Jouget detonation. As the solution is 1D, the solution is the same across all the y-coordinates, and is only different for x coordinates.
+void InitialiseDomainFromChapmanJougetDetonationSolution(Domain* domain, const ChapmanJougetDetonationSolution& sol, const double gamma);
