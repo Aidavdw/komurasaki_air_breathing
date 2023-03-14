38128ad2f38a2c9e5e5cc1e7c64e35372535c701
Aidavdw (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2023-03-14 20:33:15 +0900

    Changed implementatino of field quantity to use a flattened 2d vector for faster lookup

--- field_quantity.cpp
+++ field_quantity.cpp
@@ -1,10 +1,11 @@
 #include "field_quantity.h"
 #include <stdexcept>
 
-FieldQuantity::FieldQuantity(const unsigned int sizeX, const unsigned int sizeY, const bool bCell, const double initialValue)
+FieldQuantity::FieldQuantity(const unsigned int sizeX, const unsigned int sizeY, const double initialValue)
 {
-	// if it's set to be a cell, apparently according to florian it needs an additional element.
-	Resize2DVector(field, sizeX, sizeY, bCell, initialValue);
+	Resize2DVector(field, sizeX, sizeY, initialValue);
+
+	columnSize = sizeY;
 }
 
 void FieldQuantity::SetAllToValue(const double value, const EFieldQuantityBuffer bufferToWriteTo = EFieldQuantityBuffer::MAIN)
@@ -18,18 +19,22 @@ void FieldQuantity::CopyToBuffer(const EFieldQuantityBuffer from, const EFieldQu
 	auto& fromBuffer = bufferMap.at(from);
 	auto& toBuffer = bufferMap.at(to);
 
+	for (size_t i = 0; i < fromBuffer.size(); i++)
+		toBuffer[i] = fromBuffer[i];
 }
 
-void FieldQuantity::SetToValueInternal(std::vector<std::vector<double>>& Vec2D, const double value)
+inline int FieldQuantity::At(const int x, const int y)
+{
+	return x + (y*columnSize);
+}
+
+void FieldQuantity::SetToValueInternal(std::vector<double>& Vec2D, const double value)
 {
 	for (int i = 0; i < Vec2D.size(); i++)
-		for (int j = 0; j < Vec2D[0].size(); j++)
-			Vec2D[i][j] = 0.0;
+		Vec2D[i] = 0.0;
 }
 
-void FieldQuantity::Resize2DVector(std::vector<std::vector<double>>& vec2D, const unsigned int sizeX, const unsigned int sizeY, const bool bCell, const double initialValue)
+void FieldQuantity::Resize2DVector(std::vector<double>& vec2D, const unsigned int sizeX, const unsigned int sizeY, const double initialValue)
 {
-	// if it's set to be a cell, apparently according to florian it needs an additional element.
-	std::vector<double> single_row(sizeX + bCell, initialValue);
-	vec2D = std::vector<std::vector<double>>(sizeY + bCell, single_row);
+	vec2D = std::vector<double>(sizeX * sizeY, initialValue);
 }
--- field_quantity.h
+++ field_quantity.h
@@ -7,29 +7,33 @@ enum EFieldQuantityBuffer
 	RUNGEKUTTA, // to be used for runge kutta iteration. A temporary buffer.
 };
 
-// Thin wrapper around a 2d array with doubles, representing a value over an entire domain (in a grid)
+// Thin wrapper around a 2d array with doubles, representing a value over an entire domain (in a grid). It is implemented as a flattened 2d arary.
 struct FieldQuantity
 {
 	FieldQuantity()
 	{};
 
-	FieldQuantity(const unsigned int sizeX, const unsigned int sizeY, const bool bCell=false, const double initialValue = 0.);
+	FieldQuantity(const unsigned int sizeX, const unsigned int sizeY, const double initialValue = 0.);
 
 	//todo: Investigate how easy it is to switch the x/y order in which field vectors are stored, because this is going to cause a lot of headaches.
 
 	// Todo: implement the ghost cells here!
 	// The value of the field at a specific x/y place. value[x][y]. the first column is the x coordinate, the second one the y coordinate, because of compatibility with legacy compatibility.
-	std::map<EFieldQuantityBuffer, std::vector<std::vector<double>>&> bufferMap;
-	std::vector<std::vector<double>> field;
-	std::vector<std::vector<double>> rungeKuttaBuffer;
+	std::map<EFieldQuantityBuffer, std::vector<double>&> bufferMap;
+
+	std::vector<double> field;
+	std::vector<double> rungeKuttaBuffer;
 
 	// Sets all the values in the field to this value.
 	void SetAllToValue(const double value, const EFieldQuantityBuffer bufferToWriteTo=EFieldQuantityBuffer::MAIN);
 
 	void CopyToBuffer(const EFieldQuantityBuffer from, const EFieldQuantityBuffer to);
 
+	inline int At(const int x, const int y);
+
 private:
+	int columnSize = 0;
 	// Sets a specific 2d vector to a value. Useful for internal use to call on the buffers.
-	static void SetToValueInternal(std::vector<std::vector<double>>& valueField, const double value);
-	static void Resize2DVector(std::vector<std::vector<double>>& valueField, const unsigned int sizeX, const unsigned int sizeY, const bool bCell, const double initialValue);
+	static void SetToValueInternal(std::vector<double>& valueField, const double value);
+	static void Resize2DVector(std::vector<double>& valueField, const unsigned int sizeX, const unsigned int sizeY, const double initialValue);
 };
