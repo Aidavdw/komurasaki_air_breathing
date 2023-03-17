8fae9f97a7e25f791d1704dfd43ef6476e668866
Aidavdw (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2023-03-17 11:42:03 +0900

    Added mass matrix to beam element

--- beam_section.cpp
+++ beam_section.cpp
@@ -1,11 +1,40 @@
 #include "beam_section.h"
 #include <cmath>
 
-BeamSection::BeamSection(const double posX, const double width, const double thickness) :
+BeamSection::BeamSection(const double posX, const double length, const double width[2], const double thickness[2]) :
 	posX(posX),
-	b(width),
-	h(thickness)
+	length(length)
 {
-	crossSectionalArea = h * b;
-	areaMomentOfInertia = b * pow(h, 3) / 12.0;
+	// idea for refactor, place left- and right variables into little structs that represent surface properties? Right now the 2 element array works too I guess.
+
+	b[0] = width[0];
+	b[1] = width[1];
+	h[0] = thickness[0];
+	h[1] = thickness[1];
+
+	crossSectionalArea[0] = h[0] * b[0];
+	crossSectionalArea[1] = h[1] * b[1];
+	areaMomentOfInertia[0] = b[0] * pow(h[0], 3) / 12.0;
+	areaMomentOfInertia[1] = b[1] * pow(h[1], 3) / 12.0;
+
+	// Populating the mass matrix
+	massMatrix[0][0] = length * (10.0 * crossSectionalArea[1] + 3.0 * crossSectionalArea[0]) / 35.0;
+	massMatrix[0][1] = pow(length, 2) * (15.0 * crossSectionalArea[1] + 7.0 * crossSectionalArea[0]) / 420.0;
+	massMatrix[0][2] = 9.0 * length * (crossSectionalArea[1] + crossSectionalArea[0]) / 140.0;
+	massMatrix[0][3] = -pow(length, 2) * (7.0 * crossSectionalArea[1] + 6.0 * crossSectionalArea[0]) / 420.0;
+
+	massMatrix[1][0] = massMatrix[0][1];
+	massMatrix[1][1] = pow(length, 3) * (3.0 * crossSectionalArea[1] + 5.0 * crossSectionalArea[0]) / 840.0;
+	massMatrix[1][2] = -massMatrix[0][3];
+	massMatrix[1][3] = -pow(length, 3) * (crossSectionalArea[0] + crossSectionalArea[1]) / 280.0;
+
+	massMatrix[2][0] = massMatrix[0][2];
+	massMatrix[2][1] = massMatrix[1][2];
+	massMatrix[2][2] = length * (10.0 * crossSectionalArea[0] + 3.0 * crossSectionalArea[1]) / 35.0;
+	massMatrix[2][3] = -pow(length, 2) * (15.0 * crossSectionalArea[0] + 7.0 * crossSectionalArea[1]) / 420.0;
+
+	massMatrix[3][0] = massMatrix[0][3];
+	massMatrix[3][1] = massMatrix[1][3];
+	massMatrix[3][2] = massMatrix[2][3];
+	massMatrix[3][3] = massMatrix[1][1];
 }
--- beam_section.h
+++ beam_section.h
@@ -1,13 +1,17 @@
+#include <array>
 
 struct BeamSection
 {
-	struct BeamSection(const double posX, const double width, const double thickness);
+	struct BeamSection(const double posX, const double length, const double width[2], const double thickness[2]);
 
 	double posX;				// Position of the bottom left corner of the beam section relative to the root.
-	double b;					// The width of the beam section. Since this is a 2d case, this is in the circumferential direction (into the paper, so-to-speak).
-	double h;					// Thickness of the beam section. technically the 'height', therefore the odd name choice. Might change later.
+	double b[2];					// The width of the beam section's root part. Since this is a 2d case, this is in the circumferential direction (into the paper, so-to-speak).
+	double h[2];					// Thickness of the beam section. technically the 'height', therefore the odd name choice. Might change later.
+	double length;
 
-	double crossSectionalArea;	// cross-sectional area of the beam section, orthogonal to the bending plane.
-	double areaMomentOfInertia;	// the area moment of inertia I, in m^4.
+
+	double crossSectionalArea[2];	// cross-sectional area of the beam section, orthogonal to the bending plane.
+	double areaMomentOfInertia[2];	// the area moment of inertia I, in m^4.
+	double massMatrix[4][4];
 
 };
