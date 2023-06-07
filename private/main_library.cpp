34d81898f9a95f6a96b9de9b8d67d5f5ebad4d34
Aidavdw RedDevil <39270007+Aidavdw@users.noreply.github.com>
2023-06-07 17:02:21 +0900

    Moved the main simulation loop to wrapper function so that it can be bound easily.

---
+++ private/main_library.cpp
@@ -0,0 +1,120 @@
+﻿#include "main_library.h"
+#include <iostream>
+#include "sim_case.h"
+#include <chrono>
+#include "reed_valve.h"
+
+void DoSimulation(SimCase& simCase)
+{
+    // Check if domains are properly initialised
+    for (const auto& domain : simCase.domains)
+    {
+        if (!domain.second.AreAllBoundariesSet())
+            throw std::logic_error("Not all boundaries are set for domain " + domain.second.name);
+    }
+
+
+    /* DISPLAY INFORMATION ON TERMINAL */
+    auto timeAtStartOfCalculations = std::chrono::system_clock::now();
+    std::cout << "Total length of the simulation is " << simCase.simulationDuration << "s, with dt = " << simCase.dt/1000 << " ms (" << simCase.totalSimulationTimeStepCount << " steps)." << std::endl;
+    //std::cout << "CFL will be displayed every " << simCase.runtimeParameters.numberOfIterationsBetweenCFLLog << "timesteps (" << simCase.runtimeParameters.numberOfIterationsBetweenCFLLog * DT*1000 <<"ms)." << std::endl;
+    //std::cout << "Field properties will be exported every " << simCase.runtimeParameters.numberOfIterationsBetweenDataExport << "timesteps (" << simCase.runtimeParameters.numberOfIterationsBetweenDataExport * DT*1000 <<"ms)." << std::endl;
+    
+    /* INITIAL CONDITIONS ON DOMAINS */
+    simCase.ApplyInitialConditionsToDomainsAndValves();
+    
+    std::cout << "Starting time loop." << std::endl;
+    /* START TIME LOOP */
+    for (int timeStepNumber = 1; timeStepNumber < simCase.totalSimulationTimeStepCount; ++timeStepNumber)
+    {
+#ifdef _DEBUG
+        std::cout << "Advanced to time step " << timeStepNumber << " [ t = " << timeStepNumber*simCase.dt << "]" << std::endl;
+#endif
+        // Set the starting runge-kutta conditions to be that of the solution of the previous time step.
+        for (auto& domainIter : simCase.domains)
+        {
+            Domain& domain = domainIter.second;
+            domain.CopyFieldQuantitiesToBuffer(EFieldQuantityBuffer::CURRENT_TIME_STEP, EFieldQuantityBuffer::RUNGE_KUTTA);
+        }
+        
+        /* 4TH ORDER RUNGE-KUTTA PREDICTOR-CORRECTOR LOOP (iterate 4 times)*/
+        for (int rungeKuttaIterationNumber = 0; rungeKuttaIterationNumber < simCase.solverSettings.rungeKuttaOrder; ++rungeKuttaIterationNumber)
+        {
+#ifdef _DEBUG
+            std::cout << "RK iter " << rungeKuttaIterationNumber << " on time step " << timeStepNumber << " [ t = " << timeStepNumber*simCase.dt << "]" << std::endl;
+#endif
+            // Set the ghost cells values. Note that this is deliberately done in a separate loop so that the runge-kutta operation is sure to be finished before reading into ghost cells.
+            for (auto& domainIter : simCase.domains)
+            {
+                Domain& domain = domainIter.second;
+                domain.UpdateGhostCells();
+                domain.PopulateFlowDeltaBuffer(simCase.dt);
+            }
+
+#ifdef _DEBUG
+            std::cout << "Updating deflections, and calculating flow deltas for valves." << std::endl;
+#endif
+            for (IValve& valve : simCase.valves)
+            {
+                // Update the deflections
+                valve.UpdateValveState();
+
+                // Compute mean pressure on each side and mass-flow rate at valve
+                valve.CalculateFlow();
+            }
+
+            // Async await until both the buffers have been set for all the FieldQuantities in a domain. Easiest way to do this; wait until they're all finished.
+
+#ifdef _DEBUG
+            std::cout << "Adding valve buffer terms to source cells." << std::endl;
+#endif
+            for (IValve& valve : simCase.valves)
+            {
+                // Add the delta due to the valve sourcing into the delta flow buffer.
+                valve.AddBufferTermsToSourceCells(EFieldQuantityBuffer::FLUX);
+            }
+
+#ifdef _DEBUG
+            std::cout << "Setting next-time-step values based on the runge kutta and delta buffer contents." << std::endl;
+#endif
+            for (auto& domainIter : simCase.domains)
+            {
+                
+                Domain& domain = domainIter.second;
+                domain.SetNextTimeStepValuesBasedOnRungeKuttaAndDeltaBuffers(rungeKuttaIterationNumber);
+                domain.EmptyFlowDeltaBuffer();
+            }
+            
+            for (IValve& valve : simCase.valves)
+            {
+                valve.EmptyBuffer();
+            }
+        } /* END OF THE RUNGE-KUTTA LOOP */
+
+#ifdef _DEBUG
+        std::cout << "Taking the 'next time step' solution of the last runge-kutta iteration as the solution for the new time step." << std::endl;
+#endif
+        for (auto& domainIter : simCase.domains)
+        {
+            Domain& domain = domainIter.second;
+            domain.CopyFieldQuantitiesToBuffer(EFieldQuantityBuffer::NEXT_TIME_STEP, EFieldQuantityBuffer::CURRENT_TIME_STEP);
+        }
+
+        // todo: export flow data of domains
+        // todo: export valve data
+        //todo: implement displaying/calculating cfl
+    } // END OF TIME LOOP
+
+#ifdef _DEBUG
+    std::cout << "End of time loop!" << std::endl;
+#endif
+
+    /* FINAL INFORMATION DISPLAY AT END OF SIMULATION */
+    auto timeAtFinishOfProgram = std::chrono::system_clock::now();
+    // todo: implement timing and intermediate output.
+
+
+#ifdef _DEBUG
+    std::cout << "Simulation finished \\(^-^)/" << std::endl;
+#endif
+}
---
+++ public/main_library.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+#include "sim_case.h"
+
+// Main loop of the program; using a set-up SimCase, actually doing the simulation.
+void DoSimulation(SimCase& simCase);
