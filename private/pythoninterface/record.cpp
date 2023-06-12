e9c06c762b4e4f2d7bfe16fb269f28eb29e074a3
Aidavdw RedDevil <39270007+Aidavdw@users.noreply.github.com>
2023-06-12 17:04:59 +0900

    implement records

---
+++ private/pythoninterface/record.cpp
@@ -0,0 +1,9 @@
+﻿#include "..\..\public\pythoninterface\record.h"
+
+void TwoDimensionalArrayRecord::SaveRecord()
+{
+    // First validate the pointer
+    if (!src_)
+        throw std::runtime_error("Could not verify 2d array record pointer");
+    records.push_back(*src_);
+}
